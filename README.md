544b3531ea80505fec733374d454ddbd49c57911
rafli zocky <raflizocky@gmail.com>
2025-06-24 09:37:09 +0700

    Initial commit on new branch

---
+++ README.md
@@ -0,0 +1,3 @@
+# fieldo_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fieldo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fieldo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
